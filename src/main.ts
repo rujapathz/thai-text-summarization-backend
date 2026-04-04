b80e05f5500a4c3378464a76c03d99b6fafeb50e
Rujapa Monkhontirapat <96412812+rujapathz@users.noreply.github.com>
2026-04-04 18:11:27 +0700

    Revert "feat: set up project"

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SummerizeModule } from './summerize.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(SummerizeModule);
-  await app.listen(process.env.PORT ?? 5000);
-  console.log('Application is running on: http://localhost:5000');
-}
-bootstrap();
--- src/summarize.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface Summarize {
-  text: string;
-}
